3f855d1f305431eab96dd27bf13aaa909f6936b6
szilard-molnar <mszilard86@gmail.com>
2020-04-28 16:50:37 -0400

    ajax call added but it's not working, this api key needs a few hrs before becoming active

--- script.js
+++ script.js
@@ -5,12 +5,25 @@ $(document).ready(function() {
 
         addCityToList(inputCity);
 
+        console.log(currentWeatherData(inputCity));
+
     });
 
     function addCityToList (inputCity) {
         let newRow = $("<button>").addClass("newAddedRow").text(inputCity);
         $("#previousSearches").append(newRow);
         $("#citySearch").val("");
+    }
+
+    function currentWeatherData (inputCity) {
+        $.ajax({
+            type: "GET",
+            url: "api.openweathermap.org/data/2.5/weather?q=" + inputCity +"&appid=bc27f66aec34ec713bbdaac02025b3ac",
+            dataType: "json",
+            success: function(data) {
+                console.log(data.name);
+            }
+        })
     }
 
 });
