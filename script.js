c0296118fd9d0846cf3f6df01322f05379716329
szilard-molnar <mszilard86@gmail.com>
2020-04-28 16:24:07 -0400

    dynamically created buttons added

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-    
+
+    $("#searchButton").on("click", function() {
+        let inputCity = $("#citySearch").val();
+
+        addCityToList(inputCity);
+
+    });
+
+    function addCityToList (inputCity) {
+        let newRow = $("<button>").addClass("newAddedRow").text(inputCity);
+        $("#previousSearches").append(newRow);
+        $("#citySearch").val("");
+    }
+
 });
